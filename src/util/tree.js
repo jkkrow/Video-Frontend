8ab7af2ef078db0aa697746d48bb73b189d60067
unknown <dlord4242@gmail.com>
2021-09-17 17:17:33 +0900

    TraverseTree function

--- src/util/tree.js
+++ src/util/tree.js
@@ -1,10 +1,10 @@
 export const findById = (tree, id) => {
   let currentNode = tree.root;
-  let queue = [];
+  const queue = [];
 
   queue.push(currentNode);
 
-  while (queue.length > 0) {
+  while (queue.length) {
     currentNode = queue.shift();
 
     if (currentNode.id === id) return currentNode;
@@ -18,18 +18,36 @@ export const findById = (tree, id) => {
 
 export const findByChildId = (tree, id) => {
   let currentNode = tree.root;
-  let queue = [];
+  const queue = [];
 
-  queue.push(currentNode);
-
-  while (queue.length > 0) {
+  while (queue.length) {
     currentNode = queue.shift();
 
-    if (currentNode.children.find((item) => item?.id === id)) return currentNode;
+    if (currentNode.children.find((item) => item?.id === id))
+      return currentNode;
 
     if (currentNode.children.length)
       currentNode.children.forEach((child) => queue.push(child));
   }
 
   return null;
+};
+
+export const traverseTree = (tree) => {
+  let currentNode = tree.root;
+  const queue = [];
+  const allNodes = [];
+
+  queue.push(currentNode);
+
+  while (queue.length) {
+    currentNode = queue.shift();
+
+    allNodes.push(currentNode);
+
+    if (currentNode.children.length)
+      currentNode.children.forEach((child) => queue.push(child));
+  }
+
+  return allNodes;
 };
