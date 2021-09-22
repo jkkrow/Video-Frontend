7ca6d63aa26a0ded2e431a17311a9a3b1601da61
JK <dlord4242@gmail.com>
2021-09-22 20:29:29 +0900

    LoadingCard number logic

--- src/pages/Video/VideoListPage.js
+++ src/pages/Video/VideoListPage.js
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-// import VideoTree from "components/Video/VideoTree";
 import LoadingCard from "components/UI/Loader/LoadingCard";
 import "./VideoListPage.css";
 
-const ITEMS = [1, 1, 1, 1, 1, 1, 1, 1];
-
 const VideoListPage = () => {
   const [loadingStatus, setLoadingStatus] = useState(true);
+  const [loaderNumber, setLoaderNumber] = useState([1]);
+
+  const listRef = useRef();
+  const itemRef = useRef();
+
+  useEffect(() => {
+    const listWidth = listRef.current.offsetWidth;
+    const itemWidth = itemRef.current.offsetWidth;
+
+    let rows = Math.floor(listWidth / itemWidth);
+
+    if (rows === 1) rows = 3;
+
+    setLoaderNumber(Array.from(Array(rows * 3)));
+  }, []);
 
   return (
     <div className="video-list-page">
       <div className="video-carousel">
         <LoadingCard on={loadingStatus} />
       </div>
-      <div className="video-list">
-        {ITEMS.map((item, index) => (
-          <div key={index} className="video-item">
+      <div className="video-list" ref={listRef}>
+        {loaderNumber.map((item, index) => (
+          <div key={index} className="video-item" ref={itemRef}>
             <LoadingCard
               on={loadingStatus}
               detail
